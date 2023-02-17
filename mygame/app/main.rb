0c0064344b7c536e998135ae27eeb322f46e12e5
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-17 15:09:32 +0900

    Initialize DragonSkeleton

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,12 @@
+require 'smaug.rb'
+
 def tick(args)
+  setup(args) if args.tick_count.zero?
   args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
   args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
   args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
+end
+
+def setup(args)
+  DragonSkeleton.add_to_top_level_namespace unless Object.const_defined? :Animations
 end
