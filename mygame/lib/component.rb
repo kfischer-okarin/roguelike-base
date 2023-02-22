4fcacd123e15d04b6e3ebe1928d8a3bf1ba8952d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-22 21:57:28 +0900

    Write some conceptual comments about components

--- mygame/lib/component.rb
+++ mygame/lib/component.rb
@@ -1,4 +1,10 @@
 module Component
+  # A component is a particular lens through which we view an entity.
+  # Possible kinds of components:
+  # - A Role an entity plays (The entity acts as a ...)
+  # - The type/class of an entity (The entity is a ...)
+  # - A distinctive property of an entity (The entity has a/is ...)
+  # - A capability of an entity (The entity can ...)
   class << self
     def define(name, &block)
       @definitions ||= {}
