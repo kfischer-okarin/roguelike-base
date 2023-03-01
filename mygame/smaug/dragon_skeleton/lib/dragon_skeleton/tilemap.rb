7d1ea42f182e2c26e4a17481b1ce8e19cfdcc87d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-03-01 19:40:31 +0900

    Update DragonSkeleton lib to 2023.03.01.1

--- mygame/smaug/dragon_skeleton/lib/dragon_skeleton/tilemap.rb
+++ mygame/smaug/dragon_skeleton/lib/dragon_skeleton/tilemap.rb
@@ -15,13 +15,13 @@ module DragonSkeleton
     # The y coordinate of the bottom left corner of the tilemap
     attr_accessor :y
     # The width of each cell in the tilemap
-    attr_reader :grid_w
-    # The height of each cell in the tilemap
-    attr_reader :grid_h
-    # The width of the tilemap in cells
     attr_reader :cell_w
-    # The height of the tilemap in cells
+    # The height of each cell in the tilemap
     attr_reader :cell_h
+    # The width of the tilemap in cells
+    attr_reader :grid_w
+    # The height of the tilemap in cells
+    attr_reader :grid_h
 
     # Creates a new tilemap.
     #
@@ -57,6 +57,24 @@ module DragonSkeleton
     # Renders the tilemap to the given outputs / render target.
     def render(outputs)
       outputs.primitives << @primitive
+    end
+
+    # Converts a position to grid coordinates.
+    def to_grid_coordinates(position)
+      {
+        x: (position.x - @x).idiv(@cell_w),
+        y: (position.y - @y).idiv(@cell_h)
+      }
+    end
+
+    # Returns the rectangle of the cell at the given grid coordinates.
+    def cell_rect(grid_coordinates)
+      {
+        x: @x + (grid_coordinates.x * @cell_w),
+        y: @y + (grid_coordinates.y * @cell_h),
+        w: @cell_w,
+        h: @cell_h
+      }
     end
 
     class RenderedPrimitive # :nodoc: Internal class responsible for rendering the tilemap.
