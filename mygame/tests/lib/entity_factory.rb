b35caaa8859ecf8b677267dee05280fdc64d5d4c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-03-03 15:56:58 +0900

    tests: Add test to ensure prototypes don't change

--- mygame/tests/lib/entity_factory.rb
+++ mygame/tests/lib/entity_factory.rb
@@ -18,3 +18,26 @@ def test_entity_factory_instantiate(_args, assert)
 
   assert.has_attributes! entity, x: 200, y: 100, hp: 100
 end
+
+
+def test_entity_factory_does_not_change_prototypes(_args, assert)
+  components = ComponentDefinitions.new
+  components.define(:map_location) do
+    attribute :x
+    attribute :y
+  end
+  components.define(:health) do
+    attribute :hp
+  end
+  entity_store = EntityStore.new component_definitions: components
+  prototypes = {
+    orc: { components: %i[map_location], x: 100, y: 100 }
+  }
+  factory = EntityFactory.new entity_store: entity_store, prototypes: prototypes
+
+  factory.instantiate :orc, with_components: %i[health], x: 200, hp: 100
+
+  assert.equal! prototypes, {
+    orc: { components: %i[map_location], x: 100, y: 100 }
+  }
+end
