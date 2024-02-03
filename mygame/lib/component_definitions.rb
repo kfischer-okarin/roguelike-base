94f7baac229a2a5e2940e9e84a86e8a5a1be76fa
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-02-03 15:43:35 +0900

    Improve component object extension to have faster attribute access

--- mygame/lib/component_definitions.rb
+++ mygame/lib/component_definitions.rb
@@ -6,6 +6,7 @@ class ComponentDefinitions
   def define(name, &block)
     @definitions[name] = Module.new do
       @name = name
+      @attributes = {}
       extend Component
     end
     @definitions[name].class_eval(&block)
@@ -25,16 +26,8 @@ class ComponentDefinitions
 
   module Component
     def attribute(name, default: nil)
-      component_name = @name
       default_values[name] = default if default
-
-      define_method(name) do
-        @entity_component_data[component_name][name]
-      end
-
-      define_method("#{name}=") do |value|
-        @entity_component_data[component_name][name] = value
-      end
+      @attributes[name] = { default: default }
     end
 
     def entity_attribute(name)
@@ -51,6 +44,21 @@ class ComponentDefinitions
 
     def default_values
       @default_values ||= {}
+    end
+
+    def extend_object(obj)
+      super(obj)
+
+      component_data = obj.instance_variable_get(:@entity_component_data)[@name]
+      @attributes.each do |name, options|
+        obj.define_singleton_method(name) do
+          component_data[name]
+        end
+
+        obj.define_singleton_method("#{name}=") do |value|
+          component_data[name] = value
+        end
+      end
     end
   end
 end
