c9343ae70e43518e66de48ce0bc26fb27412c03c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-02-04 22:28:12 +0900

    Remove Game#tick

--- mygame/app/game.rb
+++ mygame/app/game.rb
@@ -15,21 +15,8 @@ class Game
     @player_entity.map
   end
 
-  def tick(input_actions) # -> perform_player_action
-    process_input input_actions
-    @world.tick if @player_entity.action
-  end
-
   def perform_player_action(action)
     @player_entity.action = action
     @world.tick
-  end
-
-  private
-
-  def process_input(input_actions)
-    return unless input_actions[:move]
-
-    @player_entity.action = { type: :move, x: input_actions[:move][:x], y: input_actions[:move][:y] }
   end
 end
