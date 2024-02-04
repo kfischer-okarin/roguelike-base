2a62f1ec7399fc8914ca4423a869169b6e9c1e37
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-02-04 21:57:53 +0900

    Check values for duppability

--- mygame/lib/deep_dup.rb
+++ mygame/lib/deep_dup.rb
@@ -1,12 +1,58 @@
-def deep_dup(obj)
-  case obj
-  when Array
-    obj.map { |item| deep_dup(item) }
-  when Hash
-    obj.transform_values { |value| deep_dup(value) }
-  when Numeric, Symbol, TrueClass, FalseClass, NilClass
-    obj
-  else
-    obj.dup
+module DeepDup
+  class << self
+    def dup(obj)
+      case obj
+      when Array
+        obj.map { |item| dup(item) }
+      when Hash
+        obj.transform_values { |value| dup(value) }
+      when Numeric, Symbol, TrueClass, FalseClass, NilClass
+        obj
+      else
+        obj.dup
+      end
+    end
+
+    def raise_unless_duppable!(value)
+      case value
+      when Array, Hash
+        DuppableChecker.new(value).check!
+      end
+    end
+  end
+
+  class DuppableChecker
+    def initialize(value)
+      @value = value
+      @object_ids = {}
+    end
+
+    def check!
+      check_recursive_references!(@value)
+    end
+
+    private
+
+    def check_recursive_references!(value)
+      case value
+      when Array
+        raise_if_object_id_seen_before!(value)
+        @object_ids[value.object_id] = true
+        value.each { |item| check_recursive_references!(item) }
+      when Hash
+        raise_if_object_id_seen_before!(value)
+        @object_ids[value.object_id] = true
+        value.each { |key, item|
+          check_recursive_references!(key)
+          check_recursive_references!(item)
+        }
+      end
+    end
+
+    def raise_if_object_id_seen_before!(value)
+      return unless @object_ids.key?(value.object_id)
+
+      raise ArgumentError, "Value #{value} is not deep duppable (contains recursive references)"
+    end
   end
 end
--- mygame/tests/lib/deep_dup.rb
+++ mygame/tests/lib/deep_dup.rb
@@ -1,6 +1,6 @@
 def test_deep_dup_array(_args, assert)
   original = [1, [2, 3], { a: 4 }]
-  copy = deep_dup(original)
+  copy = DeepDup.dup(original)
 
   assert.equal! original, copy
 
@@ -12,7 +12,7 @@ end
 
 def test_deep_dup_hash(_args, assert)
   original = { a: 1, b: [2, 3], c: { d: 4 } }
-  copy = deep_dup(original)
+  copy = DeepDup.dup(original)
 
   assert.equal! original, copy
 
@@ -24,10 +24,32 @@ end
 
 def test_deep_dup_string(_args, assert)
   original = 'hello'
-  copy = deep_dup(original)
+  copy = DeepDup.dup(original)
 
   assert.equal! original, copy
 
   original << ' world'
   assert.equal! copy, 'hello'
+end
+
+def test_raise_unless_duppable!(_args, assert)
+  deep_duppable_values = [1, 1.2, 'string', :a, true, false, nil, [{ a: [3] }], { a: [1, 2] }]
+
+  deep_duppable_values.each do |value|
+    DeepDup.raise_unless_duppable!(value)
+  end
+
+  recursive_values = [[], {}, {}]
+  recursive_values[0] << recursive_values[0]
+  recursive_values[1][:a] = recursive_values[1]
+  recursive_values[2][recursive_values[2]] = 1
+
+  recursive_values.each do |value|
+    DeepDup.raise_unless_duppable!(value)
+    raise "Expected to raise an error for value #{value}"
+  rescue ArgumentError => e
+    assert.equal! e.message, "Value #{value} is not deep duppable (contains recursive references)"
+  end
+
+  assert.ok!
 end
